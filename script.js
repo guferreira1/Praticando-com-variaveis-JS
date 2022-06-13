320f75bfff1efb05fc7af6c730cb645a543d6a80
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-06-13 17:36:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = 'Gustavo'
+let sobrenome = 'Ferreira'
+let anoDeNascimento = 1995
+let cidade = 'São Paulo'
+
+let idade = 2022-anoDeNascimento
+
+console.log (`Eu sou o ${nome} ${sobrenome}, tenho ${idade} e moro em ${cidade}`)
